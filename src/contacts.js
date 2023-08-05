57ae605d6d8fec13fc0793b1b1106852f16349a7
Eseha Joseph Ilerioluwa <113183844+jossymono@users.noreply.github.com>
2023-08-06 00:42:18 +0100

    Initial commit

---
+++ src/contacts.js
@@ -0,0 +1,25 @@
+const contacts = [
+  {
+    name: "Jossymono",
+    imgURL:
+      "../images/jossymono.jpg",
+    phone: "+234 704 254 9915",
+    email: "josepheseha335@gmail.com"
+  },
+  {
+    name: "Jack Bauer",
+    imgURL:
+      "../images/jossymono.jpg",
+    phone: "+987 654 321",
+    email: "jack@nowhere.com"
+  },
+  {
+    name: "Chuck Norris",
+    imgURL:
+      "../images/jossymono.jpg",
+    phone: "+918 372 574",
+    email: "gmail@chucknorris.com"
+  }
+];
+
+export default contacts;
